d3259839dec2541c8f2bfed95a03b570ec31e926
Resul Selmani <resul@frakton.com>
2023-05-11 01:03:32 +0200

    feature: add prompt cards in homepage with the prompts along with the copy button, add the profile routing along with the prompts based on user and add edit & delete buttons

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,10 +3,6 @@ import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
-export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
-};
 
 const RootLayout = ({ children }) => {
   return (
---
+++ app/profile/page.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name="My profile"
+      desc="welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
