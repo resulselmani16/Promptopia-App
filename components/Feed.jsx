97e253651c9916f96bde8cd651b660f9d8e00cda
Resul Selmani <resul@frakton.com>
2023-05-18 14:04:12 +0200

    Implement search features, search by tag, content or username

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,8 +18,9 @@ const PromptsCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-  const handleSearchChange = (e) => {};
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -29,6 +30,34 @@ const Feed = () => {
     };
     fetchPosts();
   }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -41,7 +70,7 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptsCardList data={posts} handleTagClick={() => {}} />
+      <PromptsCardList data={posts} handleTagClick={handleTagClick} />
     </section>
   );
 };
